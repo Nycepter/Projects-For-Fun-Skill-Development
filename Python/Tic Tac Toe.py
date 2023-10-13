f5a2153eb4da508017459ecd2f182b4c6ec59c57
Aaron Jenkins <97716896+Nycepter@users.noreply.github.com>
2023-10-13 03:17:17 -0500

    projects

---
+++ Python/Tic Tac Toe.py
@@ -0,0 +1,58 @@
+line1 = ["⬜️", "️⬜️", "️⬜️"]
+line2 = ["⬜️", "⬜️", "️⬜️"]
+line3 = ["⬜️️", "⬜️️", "⬜️️"]
+board = [line1, line2, line3]
+print("    A     B     C")
+print(f"1{line1}\n2{line2}\n3{line3}")
+turn_counter = 0
+
+while True:
+
+    position = input("Where do you want to go X? \n")
+    letter = position[0].lower()
+    abc = ["a", "b", "c"]
+    letter_index = abc.index(letter)
+    number_index = int(position[1]) - 1
+    if board[number_index][letter_index] == "X" or board[number_index][letter_index] == "O":
+        print("This space is already taken, please choose another")
+        continue
+    else:
+        board[number_index][letter_index] = "X"
+        turn_counter += 1
+        print("    A     B     C")
+        print(f"1{line1}\n2{line2}\n3{line3}")
+
+    if (line1[0] == "X" and line2[0] == "X" and line3 == "X") or (line1[1] == "X" and line2[1] == "X" and line3[1] == "X") or (line1[2] == "X" and line2[2] == "X" and line3[2] == "X") or (line1[0] == "X" and line1[1] == "X" and line1[2] == "X") or (line2[0] == "X" and line2[1] == "X" and line2[2] == "X") or (line3[0] == "X" and line3[1] == "X" and line3[2] == "X") or (line1[0] == "X" and line2[1] == "X" and line3[2] == "X") or (line1[2] == "X" and line2[1] == "X" and line3[0] == "X"):
+        print("Player X wins!")
+        exit()
+
+    elif turn_counter == 9:
+        print("The game is a Draw!")
+        exit()
+
+    while True:
+
+        position = input("Where do you want to go O? \n")
+
+        letter = position[0].lower()
+        abc = ["a", "b", "c"]
+        letter_index = abc.index(letter)
+        number_index = int(position[1]) - 1
+        if board[number_index][letter_index] == "X" or board[number_index][letter_index] == "O":
+            print("This space is already taken, please choose another")
+            continue
+
+        else:
+            board[number_index][letter_index] = "O"
+            turn_counter += 1
+            print("    A     B     C")
+            print(f"1{line1}\n2{line2}\n3{line3}")
+
+        if (line1[0] == "O" and line2[0] == "O" and line3 == "O") or (line1[1] == "O" and line2[1] == "O" and line3[1] == "O") or (line1[2] == "O" and line2[2] == "O" and line3[2] == "O") or (line1[0] == "O" and line1[1] == "O" and line1[2] == "O") or (line2[0] == "O" and line2[1] == "O" and line2[2] == "O") or (line3[0] == "O" and line3[1] == "O" and line3[2] == "O") or (line1[0] == "O" and line2[1] == "O" and line3[2] == "O") or (line1[2] == "O" and line2[1] == "O" and line3[0] == "O"):
+            print("Player O wins!")
+            exit()
+        elif turn_counter == 9:
+            print("The game is a Draw!")
+            exit()
+        else:
+            break
---
+++ Python/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+print("   A     B     C")
+print(f"1:{line1}\n2:{line2}\n3:{line3}")
