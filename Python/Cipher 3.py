9bb8d36448a0779a0831d401f0d91b40dc2dfaea
Aaron Jenkins <97716896+Nycepter@users.noreply.github.com>
2023-10-30 03:40:30 -0500

    Update Cipher 3.py

--- Python/Cipher 3.py
+++ Python/Cipher 3.py
@@ -54,6 +54,29 @@ def encrypt(message, shifter):
 
 
 def decrypt(message, shifter):
+    """
+    _summary_
+
+    Args:
+    message (_Str_): The message that the user wants to decrypt.
+    shifter (_Int_): The initial amount that the letters were shifted by.
+
+    Description:
+    This code defines a function called "decrypt" that takes two parameters: "message" and "shifter". 
+    The code first converts the "message" into a list called "list_message". It then initializes an empty list called "encrypted_message" to store the decrypted letters. 
+    The variable "shifted_letters" is set to 1 to keep track of the number of letters that have been shifted. 
+    The code then iterates over each letter in "list_message". 
+    If the letter is in the "alphabet" (undefined in the code), it proceeds to decrypt it. 
+    If the "shifted_letters" is divisible by 2, it calculates the shifted index by subtracting the "shifter" minus the "shifted_letters" from the index of the letter. 
+    Then it calculates the modulo 63 of the shifted index to ensure it remains within the range of available letters. 
+    The decrypted letter is then retrieved from the "alphabet" using the shifted index and appended to the "encrypted_message" list. 
+    The "shifted_letters" variable is incremented by 1. 
+    If the "shifted_letters" is not divisible by 2, the shifted index is calculated by adding the "shifter" plus the "shifted_letters" to the index of the letter. 
+    The rest of the process is the same as the previous case. 
+    If the letter is not in the "alphabet", it is appended as is to the "encrypted_message" list. 
+    After iterating over all the letters in "list_message", the "encrypted_message" is joined back into a string called "encrypted_message_finished". 
+    Finally, the decrypted message is printed to the console.
+    """
     list_message = list(message)
     encrypted_message = []
     shifted_letters = 1
